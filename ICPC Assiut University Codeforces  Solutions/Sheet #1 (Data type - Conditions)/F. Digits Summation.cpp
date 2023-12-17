1bdf1bf9544e7639da1a6e688651f4f99c31a2a5
Md Samiul Alam Shajal <97245391+amiekazob@users.noreply.github.com>
2023-12-17 19:03:26 +0600

    Add files via upload

---
+++ ICPC Assiut University Codeforces  Solutions/Sheet #1 (Data type - Conditions)/F. Digits Summation.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  
+ 	long long N, M;
+ 	cin >> N >> M;
+ 	int res1 = N % 10;
+ 	int res2 = M % 10;
+ 	cout << res1 + res2 << "\n";
+ 	return 0;
+}

649c8e7159b58d08c7019fa4cc0c8c3cb68c9ca4
Md Samiul Alam Shajal <97245391+amiekazob@users.noreply.github.com>
2023-12-17 19:11:15 +0600

    Update F. Digits Summation.cpp

--- ICPC Assiut University Codeforces  Solutions/Sheet #1 (Data type - Conditions)/F. Digits Summation.cpp
+++ ICPC Assiut University Codeforces  Solutions/Sheet #1 (Data type - Conditions)/F. Digits Summation.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main() {
   
-  long long N, M;
-  cin >> N >> M;
-  int res1 = N % 10;
-  int res2 = M % 10;
-  cout << res1 + res2 << "\n";
-  return 0;
+ 	long long N, M;
+ 	cin >> N >> M;
+ 	int res1 = N % 10;
+ 	int res2 = M % 10;
+ 	cout << res1 + res2 << "\n";
+ 	return 0;
 }

9961a2728b9c4ab093b92b89d2b044b4608f011b
Md Samiul Alam Shajal <97245391+amiekazob@users.noreply.github.com>
2023-12-18 12:14:08 +0600

    Update J. Multiples.cpp

--- ICPC Assiut University Codeforces  Solutions/Sheet #1 (Data type - Conditions)/J. Multiples.cpp
+++ ICPC Assiut University Codeforces  Solutions/Sheet #1 (Data type - Conditions)/J. Multiples.cpp
@@ -18,7 +18,7 @@ int main() {
 /* Discussion 
 
 A will be multiple of B , if B divides A ( with 0 reminder )  or vice verse.
-Take mod of two numbers if we got 0 reminder print "Multiples" otherwise "No Multiples"
+Take mod of two numbers if we got 0 as reminder print "Multiples" otherwise "No Multiples"
 
 link - 1 : https://byjus.com/maths/what-is-a-multiple/
 link - 2 : https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/what-is-modular-arithmetic

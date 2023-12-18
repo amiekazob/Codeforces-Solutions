c1eab0aa6c9dd286bc005a7b7343147d8f14738d
Md Samiul Alam Shajal <97245391+amiekazob@users.noreply.github.com>
2023-12-18 10:33:09 +0600

    Update E. Area of a Circle.cpp

--- ICPC Assiut University Codeforces  Solutions/Sheet #1 (Data type - Conditions)/E. Area of a Circle.cpp
+++ ICPC Assiut University Codeforces  Solutions/Sheet #1 (Data type - Conditions)/E. Area of a Circle.cpp
@@ -6,18 +6,13 @@ using namespace std;
 	
 int main(){
 			
-	double r ; 
-	    
-	cin >> r;
-	    
-	const double pi = 3.141592653;
-	    
-	double area =  pi * r * r;
-	    
-	cout << fixed << setprecision(9) << area << "\n";
-		
-	return 0;
-	
+  double r ; 
+  cin >> r;
+  const double pi = 3.141592653;
+  double area =  pi * r * r;
+  cout << fixed << setprecision(9) << area << "\n";
+  		
+  return 0;	
 }
 	
 //code ends here.
@@ -65,4 +60,3 @@ Conclusion : If you still don't understand it, don't think too much .Ask your do
 	
 */
 	
-

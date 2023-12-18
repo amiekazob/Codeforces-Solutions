f16f4e23bd32ee4fa49613716a6355fb76023449
Md Samiul Alam Shajal <97245391+amiekazob@users.noreply.github.com>
2023-12-18 10:21:23 +0600

    Add files via upload

---
+++ ICPC Assiut University Codeforces  Solutions/Sheet #1 (Data type - Conditions)/G. Summation from 1 to N.cpp
@@ -0,0 +1,30 @@
+// code starts from here
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){	
+
+  long long n;
+  cin >> n;
+  long long res =  ((n*n) + n) / 2;
+  cout<< res << "\n";   	    
+  return 0;	
+}
+// code ends from here
+
+/* soluation hint
+
+long long res = 0;
+for (int i = 0; i < n ; i++) {
+  res+=arr[i];
+}
+
+this soluation won't work.because a loop able to do at most 10^8 operation.here
+in the worst case you will given a number of 10^9 .If the limit of the loop is
+is 10^8 , how would it calculate till 10^9 ? it won't.You will get an wrong answer.
+so we can use a math formula only for this given problem.
+
+Did you understand why did we use 'long long res' to calculate the answer insted of 'int res'.
+ask  your teacher, friends,  senior, or internet.
+
+*/

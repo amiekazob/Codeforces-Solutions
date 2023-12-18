ee8432c94487bddc4d7a5d253060e464158d3a64
Md Samiul Alam Shajal <97245391+amiekazob@users.noreply.github.com>
2023-12-18 10:39:23 +0600

    Update C. Simple Calculator.cpp

--- ICPC Assiut University Codeforces  Solutions/Sheet #1 (Data type - Conditions)/C. Simple Calculator.cpp
+++ ICPC Assiut University Codeforces  Solutions/Sheet #1 (Data type - Conditions)/C. Simple Calculator.cpp
@@ -3,22 +3,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
 int main(){
 		
-    int x , y; 
-    
-    cin >> x >> y;
-    
-    cout <<x <<" + "<<y << " = " <<x+y <<"\n";
-    cout <<x <<" * "<<y << " = " <<(long long)x*y <<"\n";
-    cout <<x <<" - "<<y << " = " <<x-y <<"\n";
-	
-    return 0;
+  int x , y;     
+  cin >> x >> y;   
+  cout <<x <<" + "<<y << " = " << x+y <<"\n";
+  cout <<x <<" * "<<y << " = " <<(long long)x*y <<"\n";
+  cout <<x <<" - "<<y << " = " << x-y <<"\n";
+
+  return 0;
 }
 
 //code ends here.
-
 
 
 
@@ -35,52 +31,30 @@ As this is a unrated problem you won't able to see others test cases which cause
 Method 1 -  In the problem input connstraints are ,   X, Y (1  =  X, Y  =  10^5 ).
 
 Now Imagine a worst  case for this problem acroding to the connstraints : 
-
 X = 10^5 and Y = 10^5. Now while multiplying  X and Y
-
 cout <<x <<" * "<<y << " = " <<x*y <<"\n";
 
 But wait? x*y = 10^5 * 10^5 = 10^10 .
-
 Now look closely. we know the range of ' int ' is 2*10^9  . But the answer is 10^10 which is overflowing the range of 'int'.And it
 caused the wrong answer.How to fix it ? simple just typecast it into ' long long int ' or 'long long' .But how ?
-
 cout <<x <<" * "<<y << " = " <<x*y <<"\n"; // wrongs answer
-
 cout <<x <<" * "<<y << " = " <<(long long)x*y <<"\n"; // right answer.
-
-
-
-
-
 
 
 Method 2 - There is an another approch to handle it which doesn't demand typecast.
 
 take input in the form of  ' long long ' . here is the code
- 
-
 
 int main(){
 		
-    long long x , y; 
-    
-	cin >> x >> y;
-    
+    long long x , y;     
+    cin >> x >> y;    
     cout <<x <<" + "<<y << " = " <<x+y <<"\n";
     cout <<x <<" * "<<y << " = " <<x*y <<"\n";
-    cout <<x <<" - "<<y << " = " <<x-y <<"\n";
-	
-	return 0;
+    cout <<x <<" - "<<y << " = " <<x-y <<"\n";	
+    return 0;
 }
-
-
-
-
 conclusion : If you still don't understand it, don't think too much .Ask your doubts to your teacher , senior or friends or internet.
 
-
-
 */
-
 

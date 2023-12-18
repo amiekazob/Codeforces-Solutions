75238ca37cde7cf7f588b10e5ff6e93dbe0212c8
Md Samiul Alam Shajal <97245391+amiekazob@users.noreply.github.com>
2023-12-18 10:41:11 +0600

    Update B. Basic Data Types.cpp

--- ICPC Assiut University Codeforces  Solutions/Sheet #1 (Data type - Conditions)/B. Basic Data Types.cpp
+++ ICPC Assiut University Codeforces  Solutions/Sheet #1 (Data type - Conditions)/B. Basic Data Types.cpp
@@ -9,14 +9,10 @@ int main(){
   char c;
   float d;
   double e;
-  
   cin >> a >> b >> c >> d >> e;
-   
   cout << a << "\n"  << b << "\n" << c << "\n" << d << "\n" << e << "\n";
-    
-
-  return 0;
-   
+  
+  return 0;   
 }
 
 
@@ -31,24 +27,15 @@ Error  handaling :
 
 1- make sure you use the correct data type.Or you will get wrong answer.
 
-
-
 2 - A common error of this problem is   " wrong output format Expected EOLN "  even after getting the right answer.It occurs because 
 your output is not in correct  form because you are using extra white or blank spaces. Don't use any extra white spaces if it doesn't demand it.
 for example : 
-
-
 cout << a << " \n"  << b << "  \n" << c << " \n" << d << " \n" << e << " \n";  // wrong answer
 cout << a << "\n"  << b << "\n" << c << "\n" << d << "\n" << e << "\n"; // right answer
-
- 
  we got error for using extra blank spaces inside  "\n".
- 
- 
+
  fun fact :  you won't find no difference between two output in your computer.But Online Judges strictly maintain output form.
-
-
-
+ 
 */
 
 

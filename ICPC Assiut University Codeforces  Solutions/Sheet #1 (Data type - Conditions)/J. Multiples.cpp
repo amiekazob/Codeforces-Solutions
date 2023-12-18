d77ae32d64dc1f01a3150af3c7c7b2a9a7bcd8e9
Md Samiul Alam Shajal <97245391+amiekazob@users.noreply.github.com>
2023-12-18 12:13:02 +0600

    Add files via upload

---
+++ ICPC Assiut University Codeforces  Solutions/Sheet #1 (Data type - Conditions)/J. Multiples.cpp
@@ -0,0 +1,27 @@
+/* code starts here */
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+   
+  if (a % b == 0 || b % a == 0) {
+  	cout <<"Multiples"<<"\n";
+  }else {
+  	cout <<"No Multiples"<<"\n";
+  }
+  return 0;
+}
+/* code end here */
+
+/* Discussion 
+
+A will be multiple of B , if B divides A ( with 0 reminder )  or vice verse.
+Take mod of two numbers if we got 0 reminder print "Multiples" otherwise "No Multiples"
+
+link - 1 : https://byjus.com/maths/what-is-a-multiple/
+link - 2 : https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/what-is-modular-arithmetic
+
+
+*/ 

3f906b77be2ad6f5c91dfaaa1b4ce01ad8f8cfec
Md Samiul Alam Shajal <97245391+amiekazob@users.noreply.github.com>
2023-12-19 12:15:20 +0600

    Update F. Digits Summation.cpp

--- ICPC Assiut University Codeforces  Solutions/Sheet #1 (Data type - Conditions)/F. Digits Summation.cpp
+++ ICPC Assiut University Codeforces  Solutions/Sheet #1 (Data type - Conditions)/F. Digits Summation.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main() {
   long long N, M;
   cin >> N >> M;
-  int res1 = N % 10; // Extract the last digit of N
-  int res2 = M % 10; // Extract the last digit of M
-  cout << res1 + res2 << "\n";
+  int lasDig1 = N % 10; // Extract the last digit of N
+  int lasDig2 = M % 10; // Extract the last digit of M
+  cout << lasDig1 + lasDig2 << "\n";
   return 0;
 }
